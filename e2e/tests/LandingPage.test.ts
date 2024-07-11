6ca2ab79d78b3370064f89979e2b6fcf54c54aba
MAXIME GAEL <maximejordan.gael@gmail.com>
2024-07-11 11:31:34 +0200

    testing the component's inputs and transfer button. The doc import doesn't work for the moment

--- e2e/tests/LandingPage.test.ts
+++ e2e/tests/LandingPage.test.ts
@@ -1,11 +1,38 @@
 import { test, expect } from "@playwright/test";
+import path from "path";
+import fs from "fs";
 
 test("Landing page", async ({ page }) => {
   await page.goto("http://frontend:5173/");
   await page.waitForLoadState("networkidle");
 
-  await page.getByPlaceholder("Your email").fill("maxime@gmail.com");
-  await page.getByPlaceholder("Email to").fill("you@gmail.com");
-  await page.getByPlaceholder("Title").fill("ton document");
-  await page.getByPlaceholder("Message").fill("Hello, voici ton document");
+  const emailInput = page.getByPlaceholder("Your email");
+  const emailToInput = page.getByPlaceholder("Email to");
+  const titleInput = page.getByPlaceholder("Title");
+  const messageInput = page.getByPlaceholder("Message");
+  const uploadDragger = page.locator(".ant-upload-drag");
+  const transferButton = page.locator('button:has-text("Transfer")');
+
+  await expect(emailInput).toBeVisible();
+  await expect(emailToInput).toBeVisible();
+  await expect(titleInput).toBeVisible();
+  await expect(messageInput).toBeVisible();
+  await expect(uploadDragger).toBeVisible();
+  await expect(transferButton).toBeVisible();
+
+  await emailInput.fill("maxime@gmail.com");
+  await emailToInput.fill("you@gmail.com");
+  await titleInput.fill("Ton document");
+  await messageInput.fill("Hello, voici ton document");
+
+  const filePath = path.resolve(
+    "/Users/maxime/Downloads/MG-fond-noir-jpg-fotor-2024061214914.png"
+  );
+  console.log("Chemin du fichier:", filePath);
+
+  if (!fs.existsSync(filePath)) {
+    throw new Error(`Le fichier n'existe pas: ${filePath}`);
+  }
+
+  await transferButton.click();
 });
