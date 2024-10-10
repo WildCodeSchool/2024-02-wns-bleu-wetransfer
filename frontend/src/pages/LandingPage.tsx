33ec0c926abece3ae76725077a8408b3977d7569
VincentRssx <Vincent.rssx59@gmail.com>
2024-10-10 11:53:01 +0200

    start responsive

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,16 +1,25 @@
-import React, {useState} from "react";
-import {Button, Divider, Form, Input, message, Select, Upload, UploadFile,} from "antd";
-import {MailOutlined, PlusCircleFilled} from "@ant-design/icons";
+import React, { useState } from "react";
+import {
+	Button,
+	Divider,
+	Form,
+	Input,
+	message,
+	Select,
+	Upload,
+	UploadFile,
+} from "antd";
+import { MailOutlined, PlusCircleFilled } from "@ant-design/icons";
 import axios from "axios";
 import styled from "@emotion/styled";
-import {UploadChangeParam} from "antd/es/upload";
+import { UploadChangeParam } from "antd/es/upload";
 import SignInForm from "../components/Signin/SignInForm.tsx";
-import {useNavigate} from 'react-router-dom'
-import {colors} from "../_colors.ts";
+import { useNavigate } from "react-router-dom";
+import { colors } from "../_colors.ts";
 
-const {Dragger} = Upload;
-const {Item} = Form;
-const {TextArea} = Input;
+const { Dragger } = Upload;
+const { Item } = Form;
+const { TextArea } = Input;
 
 interface FormValues {
 	senderEmail: string;
@@ -22,7 +31,7 @@ interface FormValues {
 const LandingPage: React.FC = () => {
 	const [fileList, setFileList] = useState<UploadFile[]>([]);
 	const [form] = Form.useForm();
-	const navigate = useNavigate()
+	const navigate = useNavigate();
 
 	const handleBeforeUpload = (file: UploadFile) => {
 		// Validate the file type and size before uploading
@@ -77,7 +86,8 @@ const LandingPage: React.FC = () => {
 					onUploadProgress: (progressEvent) => {
 						if (progressEvent.total) {
 							const percent = Math.round(
-								(progressEvent.loaded * 100) / progressEvent.total
+								(progressEvent.loaded * 100) /
+									progressEvent.total
 							);
 							console.log(percent); // You can update the UI to show the progress
 						}
@@ -94,7 +104,9 @@ const LandingPage: React.FC = () => {
 
 	const handleChange = (info: UploadChangeParam<UploadFile>) => {
 		// Filter out the files that do not pass the validation
-		const newFileList = info.fileList.filter((file) => file.status !== "error");
+		const newFileList = info.fileList.filter(
+			(file) => file.status !== "error"
+		);
 		setFileList(newFileList);
 	};
 
@@ -104,11 +116,17 @@ const LandingPage: React.FC = () => {
 			<CardsWrapper>
 				<FormContainer background="#65558F">
 					<UploadTitle>Send files casually</UploadTitle>
-					<StyledDivider/>
-					<Form form={form} name="visitorSendFilesForm" onFinish={handleUpload}>
+					<StyledDivider />
+					<Form
+						form={form}
+						name="visitorSendFilesForm"
+						onFinish={handleUpload}
+					>
 						<Item
 							name="files"
-							rules={[{required: true, message: "Files missing"}]}
+							rules={[
+								{ required: true, message: "Files missing" },
+							]}
 						>
 							<Dragger
 								name="file"
@@ -116,13 +134,23 @@ const LandingPage: React.FC = () => {
 								beforeUpload={handleBeforeUpload}
 								onChange={handleChange}
 								fileList={fileList}
-								style={{background: "rgba(255,255,255,0.26)", height: 60}}
+								style={{
+									background: "rgba(255,255,255,0.26)",
+									height: 60,
+								}}
 							>
-								<PlusCircleFilled style={{fontSize: 20, color: colors.white}}/>
+								<PlusCircleFilled
+									style={{
+										fontSize: 20,
+										color: colors.white,
+									}}
+								/>
 								<p className="ant-upload-text">
 									Click or drag file to this area to upload
 								</p>
-								<p className="ant-upload-hint">Upload max size : 2 MB</p>
+								<p className="ant-upload-hint">
+									Upload max size : 2 MB
+								</p>
 							</Dragger>
 						</Item>
 						<Item
@@ -134,53 +162,74 @@ const LandingPage: React.FC = () => {
 								},
 							]}
 						>
-							<UploadInput placeholder="Your email" allowClear/>
+							<UploadInput placeholder="Your email" allowClear />
 						</Item>
 						<Item
 							name="receiversEmails"
 							rules={[
 								{
 									required: true,
-									message: "Please enter receiver(s) email(s)",
+									message:
+										"Please enter receiver(s) email(s)",
 								},
 							]}
 						>
 							<Select
 								mode="tags"
-								style={{width: "100%"}}
+								style={{ width: "100%" }}
 								tokenSeparators={[","]}
 								placeholder="Receivers emails"
 								defaultActiveFirstOption={false}
-								suffixIcon={<MailOutlined/>}
+								suffixIcon={<MailOutlined />}
 								open={false}
 							/>
 						</Item>
-						<Divider style={{background: "white"}}/>
+						<Divider style={{ background: "white" }} />
 						<Item name="title">
-							<UploadInput placeholder="Title" allowClear/>
+							<UploadInput placeholder="Title" allowClear />
 						</Item>
 						<Item name="message">
-							<UploadTextArea placeholder="Message" allowClear maxLength={100}/>
+							<UploadTextArea
+								placeholder="Message"
+								allowClear
+								maxLength={100}
+							/>
 						</Item>
 						<Item>
-							<TransferButton type="primary" size="large" htmlType="submit">
+							<TransferButton
+								type="primary"
+								size="large"
+								htmlType="submit"
+							>
 								Transfer Files
 							</TransferButton>
 						</Item>
 					</Form>
 				</FormContainer>
 				<FormContainer background="#ffffff">
-					<SignInForm/>
+					<SignInForm />
 				</FormContainer>
 			</CardsWrapper>
 			<SuggestionsWrapper>
-				<SuggestionText>Unlock insane features by register for <strong>free</strong> !</SuggestionText>
-				<Button style={{borderRadius: 15, color: 'whitesmoke', background: 'none'}}
-				        onClick={() => navigate("/access/pricing")}>
+				<SuggestionText>
+					Unlock insane features by register for <strong>free</strong>{" "}
+					!
+				</SuggestionText>
+				<Button
+					style={{
+						borderRadius: 15,
+						color: "whitesmoke",
+						background: "none",
+					}}
+					onClick={() => navigate("/access/pricing")}
+				>
 					See plans
 				</Button>
-				<Button type='primary' style={{borderRadius: 15}}
-				        onClick={() => navigate("/access/register")}>
+				<Button
+					type="primary"
+					style={{ borderRadius: 15 }}
+					onClick={() => navigate("/access/register")}
+				>
 					Sign Up Now !
 				</Button>
 			</SuggestionsWrapper>
@@ -189,77 +238,99 @@ const LandingPage: React.FC = () => {
 };
 
 const SuggestionText = styled.p`
-    color: whitesmoke;
-    font-size: 20px;
-    font-weight: 200;
-`
+	color: whitesmoke;
+	font-size: 20px;
+	font-weight: 200;
+	@media (max-width: 768px) {
+		font-size: calc(10px + 2px + 0vmin);
+	}
+`;
 
 const SuggestionsWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 10px
-`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 10px;
+`;
 
 const Title = styled.h1`
-    font-size: 70px;
-    color: whitesmoke;
-    font-weight: 500;
-`
+	font-size: 70px;
+	color: whitesmoke;
+	font-weight: 500;
+	@media (max-width: 768px) {
+		max-width: 100%;
+		font-size: 3.5rem;
+	}
+`;
 
 const CardsWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 50px
-`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 50px;
+	max-width: 100%;
+	@media (max-width: 768px) {
+		flex-direction: column-reverse;
+	}
+`;
 
 const LandingPageWrapper = styled.div`
-    background: #0a0025;
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-around;
-    gap: 20px;
-    padding: 20px;
+	background: #0a0025;
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-around;
+	gap: 20px;
 `;
 
 const FormContainer = styled.div<{ background?: string }>`
-    background: ${({background}) => (background ? background : "#7b5c8a")};
-    padding: 18px;
-    border-radius: 20px;
-    text-align: center;
-    width: 500px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 580px;
+	background: ${({ background }) => (background ? background : "#7b5c8a")};
+	padding: 18px;
+	border-radius: 20px;
+	text-align: center;
+	width: 500px;
+	flex: 1 1 45%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	height: 580px;
+	box-sizing: border-box;
+	@media (max-width: 768px) {
+		width: 120%;
+		height: 100%;
+	}
+	&:nth-of-type(2) {
+		@media (max-width: 768px) {
+			padding-top: 70px;
+			padding-bottom: 90px;
+		}
+	}
 `;
 
 const UploadTitle = styled.h2`
-    color: white;
+	color: white;
 `;
 
 const StyledDivider = styled(Divider)`
-    border-color: white;
-    margin: 15px;
+	border-color: white;
+	margin: 15px;
 `;
 
 const UploadInput = styled(Input)`
-    width: 100%;
+	width: 100%;
 `;
 
 const UploadTextArea = styled(TextArea)`
-    width: 100%;
+	width: 100%;
 `;
 
 const TransferButton = styled(Button)`
-    width: 100%;
-    margin-top: 16px;
-    background: #0a0025;
+	width: 100%;
+	margin-top: 16px;
+	background: #0a0025;
 `;
 
 export default LandingPage;
--- frontend/src/pages/PricingPage.tsx
+++ frontend/src/pages/PricingPage.tsx
@@ -1,22 +1,36 @@
 import styled from "@emotion/styled";
-import {Button, Card} from 'antd'
+import { Button, Card } from "antd";
 
-const {Meta} = Card
+const { Meta } = Card;
 
 interface CardContent {
-	title: string,
-	description: string
-	price: number,
-	suggested: boolean
+	title: string;
+	description: string;
+	price: number;
+	suggested: boolean;
 }
 
 const PricingPage = () => {
-
 	const cardsContent: CardContent[] = [
-		{title: 'Free', description: 'Il faut décrire le plan', price: 0, suggested: false},
-		{title: 'Premium', description: 'Encore une description', price: 10, suggested: true},
-		{title: 'Ultimate', description: 'Description du plan Ultimate', price: 1424, suggested: false}
-	]
+		{
+			title: "Free",
+			description: "Il faut décrire le plan",
+			price: 0,
+			suggested: false,
+		},
+		{
+			title: "Premium",
+			description: "Encore une description",
+			price: 10,
+			suggested: true,
+		},
+		{
+			title: "Ultimate",
+			description: "Description du plan Ultimate",
+			price: 1424,
+			suggested: false,
+		},
+	];
 
 	return (
 		<PricingPageWrapper>
@@ -27,55 +41,79 @@ const PricingPage = () => {
 						key={index}
 						style={{
 							width: 300,
-							border: plan.suggested ? '5px solid rgba(231,166,26,1)' : 'none'
+							border: plan.suggested
+								? "5px solid rgba(231,166,26,1)"
+								: "none",
 						}}
-						actions={[
-							<Button type='primary'>Subscribe</Button>
-						]}
-						title={
-							<CardTitle>{plan.title}</CardTitle>
-						}
+						actions={[<Button type="primary">Subscribe</Button>]}
+						title={<CardTitle>{plan.title}</CardTitle>}
 						extra={`$${plan.price}/month`}
 					>
 						<Meta
-							title='In this plan :'
+							title="In this plan :"
 							description={plan.description}
-							style={{height: 250}}
+							style={{ height: 250 }}
 						/>
 					</Card>
 				))}
 			</CardsContainer>
 		</PricingPageWrapper>
-	)
-}
+	);
+};
 
 const CardTitle = styled.h3`
-    font-size: 27px;
-`
+	font-size: 27px;
+`;
 
 const CardsContainer = styled.div`
-    width: 80%;
-    height: 500px;
-    display: flex;
-    justify-content: space-around;
-    gap: 20px;
-    align-items: center;
-`
+	width: 80%;
+	height: 500px;
+	display: flex;
+	justify-content: space-evenly;
+	gap: 20px;
+	align-items: center;
+	@media (max-width: 768px) {
+		/* Active le défilement horizontal en mobile */
+		width: 100%;
+		overflow-x: auto;
+		display: flex;
+		flex-wrap: nowrap; /* Pas de retour à la ligne, tout en une seule ligne */
+		scroll-snap-type: x mandatory; /* Un scroll fluide d'une carte à l'autre */
+		padding-bottom: 20px;
+
+		/* Masque les barres de défilement selon le navigateur */
+		scrollbar-width: none; /* Firefox */
+		&::-webkit-scrollbar {
+			display: none; /* Chrome, Safari */
+		}
+
+		/* Applique une taille réduite pour les cartes */
+		& > .ant-card {
+			flex: 0 0 auto;
+			width: 80%; /* Les cartes prendront 80% de l'écran */
+			scroll-snap-align: start; /* Aligne les cartes au début lors du scroll */
+			transform: scale(0.9); /* Réduit légèrement la taille des cartes */
+		}
+	}
+`;
 
 export const PricingTitle = styled.h3`
-    font-size: 30px;
-    color: white;
-`
+	font-size: 30px;
+	color: white;
+`;
 
 const PricingPageWrapper = styled.div`
-    position: relative;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-top: 40px;
-`
+	position: relative;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	margin-top: 40px;
+	@media (max-width: 768px) {
+		width: 115%;
+	}
+`;
 
-export default PricingPage
+export default PricingPage;
