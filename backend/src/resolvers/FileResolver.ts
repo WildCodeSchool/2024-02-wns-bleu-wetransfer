2c8c2e2412204abd298e18717f75406a9d998391
LuckyShuii <lucasnimes30000@gmail.com>
2024-11-06 13:28:25 +0100

    edit filename

--- backend/src/resolvers/FileResolver.ts
+++ backend/src/resolvers/FileResolver.ts
@@ -24,7 +24,6 @@ class FileResolver {
       );
 
       if (response.status !== 200) {
-        console.log("NOT FOUND")
         throw new Error("Internal server error during file deletion 1");
       }
 
@@ -33,6 +32,28 @@ class FileResolver {
       return true;
     } catch (err) {
       throw new Error("Internal server error during file deletion 2");
+    }
+  }
+
+  @Mutation(() => Boolean)
+  async editFileName(
+    @Arg("id") id: number,
+    @Arg("newName") newName: string
+  ) {
+    try {
+      const file = await File.findOneByOrFail({ id });
+
+      if (!file) {
+        throw new Error("File not found");
+      }
+
+      file.name = newName;
+
+      await file.save();
+
+      return true;
+    } catch (err) {
+      throw new Error("Internal server error during file name edit");
     }
   }
 }
